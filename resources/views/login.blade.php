3da09ca1bd3ec4b8fe32f5744daa82a053ec53ca
Jefri Manurung <jefrymanurung23@gmail.com>
2020-03-12 22:39:47 +0700

    mengubah tampilan login dengan tambahan javascript

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -45,7 +45,8 @@
             </form>
         </div>
     </div>
-
+    
+    <script type="text/javascript" src="js/main.js"></script>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
