5cc06dd66940d18707f5b6a79956f86a5db2821b
Jefri Manurung <jefrymanurung23@gmail.com>
2020-03-14 02:47:26 +0700

    memperbaharui tampilan halaman login

--- public/js/main.js
+++ public/js/main.js
@@ -18,3 +18,27 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+TweenMax.staggerFrom("body", 2, {
+	delay: 1.6,
+	y: 20,
+	opacity: 0,
+	ease: Expo.easeInOut
+}, 0.1);
+
+TweenMax.staggerFrom(".login-content", 2, {
+	delay: 1.6,
+	y: 20,
+	opacity: 0,
+	ease: Expo.easeInOut
+}, 0.1);
+
+TweenMax.from(".circle-1", 4, {
+	delay: 0.1,
+	rotation: 200,
+	transformOrigin: "50% 50%",
+	opacity: 0,
+	x: 500,
+	y: 500,
+	ease: Expo.easeInOut
+});
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,9 +14,13 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
-    <div class="container">
+	<div class="circle-1 circle"></div>
+	<div class="line-1 line"></div>
+	<div class="line-2 line"></div>
+	
+	<div class="container">
 		<div class="img">
-			<img src="img/football-player2.svg" alt="">
+			<!-- <img src="img/football-player2.svg" alt=""> -->
 		</div>
 		<div class="login-content">
 			<form action="index.html">
@@ -41,16 +45,19 @@
             	   </div>
             	</div>
             	<a href="#">Lupa Password?</a>
-            	<input type="submit" class="btn" value="Login">
+            	<input type="submit" class="btn" value="Masuk">
             </form>
         </div>
     </div>
-    
-    <script type="text/javascript" src="js/main.js"></script>
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+  
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
 </html>
