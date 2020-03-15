b842237f06f087b9bae7872a3606ff15c2b7d5c7
Jefri Manurung <jefrymanurung23@gmail.com>
2020-03-15 19:39:35 +0700

    membuat kerangka tampilan halaman schedule penyewa

---
+++ resources/views/schedule.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule for Srikandi Futsal</title>
+    
+    <!-- Vue JS -->
+    <script type="text/javascript" src="js/vue/vue.js"></script>
+    
+    <!-- bootstrap css -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    
+    <!-- jquery -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    
+    <!-- bootstrap js -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
