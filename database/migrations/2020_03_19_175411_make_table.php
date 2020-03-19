0ae23f70f959690b22cda79f0ee856726ba9571b
rivaldofez <rivaldofez@gmail.com>
2020-03-20 00:55:56 +0700

    memasukkan migration table ke sql server

--- database/migrations/2020_03_19_175411_make_table.php
+++ database/migrations/2020_03_19_175411_make_table.php
@@ -13,7 +13,53 @@ class MakeTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::dropIfExists('transaksi');
+        Schema::dropIfExists('lapangan');
+        Schema::dropIfExists('jadwal');
+        Schema::dropIfExists('operator');
+        Schema::dropIfExists('user');
+
+        Schema::create('operator', function (Blueprint $table) {
+            $table->string('kode_operator',5);
+            $table->string('nama',50);
+            $table->string('password',50);
+            $table->primary('kode_operator');
+        });
+        
+        Schema::create('user', function (Blueprint $table) {
+            $table->string('kode_user',5);
+            $table->string('nama',50);
+            $table->string('alamat',30);
+            $table->string('telepon',12);
+            $table->primary('kode_user');
+        });
+
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->string('kode_jadwal',5);
+            $table->string('jam',15);
+            $table->integer('harga')->length(20)->unsigned();
+            $table->primary('kode_jadwal');
+        });
+
+        Schema::create('lapangan', function (Blueprint $table) {
+            $table->string('kode_lapangan',5);
+            $table->string('nama',50);
+            $table->string('lokasi',20);
+            $table->string('kode_jadwal',5);
+            $table->primary('kode_lapangan');
+        });
+
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->string('kode_transaksi',5);
+            $table->string('kode_operator',5);
+            $table->string('kode_user',5);
+            $table->string('kode_lapangan',5);
+            $table->string('kode_jadwal',5);
+            $table->integer('diskon')->length(20)->unsigned();
+            $table->date('tanggal',30);
+            $table->primary('kode_transaksi');
+        });
+
     }
 
     /**
