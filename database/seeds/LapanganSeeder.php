7d20d9317fa5df806fc23ec996bc935486d06b61
rivaldofez <rivaldofez@gmail.com>
2020-04-16 02:55:30 +0700

    Seed lapangan atas

--- database/seeds/LapanganSeeder.php
+++ database/seeds/LapanganSeeder.php
@@ -11,6 +11,26 @@ class LapanganSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('lapangan')->delete();
+
+        for ($i=7; $i < 23; $i++) {
+            if($i <10 ){
+                $j = sprintf("%02d", $i);
+                $end = sprintf("%02d", $i+1);
+                $jam = $j.'.00-'.$end.'.00';
+            }else{
+                $j = $i;
+                $end = $i+1;
+                $jam = $i.'.00-'.$end.'.00';
+            } 
+
+            DB::table('lapangan')->insert([
+               'kode_lapangan' => 'LA-'.$j ,
+               'nama' => 'Lapangan Atas',
+               'lokasi' => 'Atas',
+               'kode_jadwal' => 'A'.$j,
+           ]);
+        }
+
     }
 }
