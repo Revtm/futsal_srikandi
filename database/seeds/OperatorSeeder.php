4fd8175835b3a04118a4865d333ce60714016136
rivaldofez <rivaldofez@gmail.com>
2020-04-16 02:11:32 +0700

    menambahkan seeder operator

---
+++ database/seeds/OperatorSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OperatorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
