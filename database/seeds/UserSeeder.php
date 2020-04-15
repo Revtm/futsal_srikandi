2d3dfdd3f5b6f7594912aa36e50b3b1e6928a860
rivaldofez <rivaldofez@gmail.com>
2020-04-16 03:07:28 +0700

    Menambah data seed user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,25 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('user')->insert([
+            [
+                'nama' => 'Budi Budiman',
+                'alamat' => 'Jl.Pepaya No.1',
+                'telepon' => '081122334455',
+            ],
+
+            [
+                'nama' => 'Caca Macaca',
+                'alamat' => 'Jl.Nangka No.2',
+                'telepon' => '082233445566',
+            ],
+
+            [
+                'nama' => 'Andi Surandi',
+                'alamat' => 'Jl.Durian No.3',
+                'telepon' => '083344556677',
+            ]
+
+        ]);
     }
 }
