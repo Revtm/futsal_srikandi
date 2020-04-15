69ac6963de10a78c46d0b41a02e19cafbb374dd2
rivaldofez <rivaldofez@gmail.com>
2020-04-16 03:16:02 +0700

    Menambah data pada seed transaksi

--- database/seeds/TransaksiSeeder.php
+++ database/seeds/TransaksiSeeder.php
@@ -11,6 +11,60 @@ class TransaksiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('transaksi')->insert([
+            [
+                'kode_operator' => '1',
+                'kode_user' => '1',
+                'kode_lapangan' => 'LA-13',
+                'kode_jadwal' => 'A13',
+                'diskon' => '10000',
+                'tanggal' => '2020-04-16',
+            ],
+
+            [
+                'kode_operator' => '1',
+                'kode_user' => '2',
+                'kode_lapangan' => 'LA-13',
+                'kode_jadwal' => 'A13',
+                'diskon' => '10000',
+                'tanggal' => '2020-04-16',
+            ],
+
+            [
+                'kode_operator' => '1',
+                'kode_user' => '3',
+                'kode_lapangan' => 'LT-13',
+                'kode_jadwal' => 'T13',
+                'diskon' => '5000',
+                'tanggal' => '2020-04-16',
+            ],
+
+            [
+                'kode_operator' => '1',
+                'kode_user' => '1',
+                'kode_lapangan' => 'LA-07',
+                'kode_jadwal' => 'A07',
+                'diskon' => '1000',
+                'tanggal' => '2020-04-15',
+            ],
+
+            [
+                'kode_operator' => '1',
+                'kode_user' => '2',
+                'kode_lapangan' => 'LB-22',
+                'kode_jadwal' => 'A22',
+                'diskon' => '3000',
+                'tanggal' => '2020-04-15',
+            ],
+
+            [
+                'kode_operator' => '1',
+                'kode_user' => '3',
+                'kode_lapangan' => 'LT-15',
+                'kode_jadwal' => 'T15',
+                'diskon' => '50000',
+                'tanggal' => '2020-04-15',
+            ]
+        ]);
     }
 }
