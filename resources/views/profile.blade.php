c4f7422c0522ca879b6ba70c3eeb368df262ee9f
rivaldofez <rivaldofez@gmail.com>
2020-04-22 02:22:59 +0700

    menambah asset, css dan seection pada halaman profiile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -39,3 +39,41 @@
 			<li><a href="#list">Our Vehicles</a></li>
 		</nav>
 	</header>
+
+
+	<section class="banner-area">
+		<div class="img-area"></div>
+		<div class="banner-text">
+			<h1>Srikandi Rent</h1>
+			<h3>Make it Easy...</h3>
+			<a href="#" class="btn">Learn More</a>
+		</div>
+	</section>
+
+	<section class="services-area" id="services">
+		<h3 class="header-text">Our Services</h3>
+		<p>We provide cool vehicle for you</p>
+		<div class="content-area">
+			<div class="single-service">
+				<div class="icon-area">
+					<i class="fas fa-car"></i>
+				</div>
+				<h2>Car</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, inventore.</p>
+			</div>
+			<div class="single-service">
+				<div class="icon-area">
+					<i class="fas fa-motorcycle"></i>
+				</div>
+				<h2>Motorcycle</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, inventore.</p>
+			</div>
+			<div class="single-service">
+				<div class="icon-area">
+					<i class="fas fa-bicycle"></i>
+				</div>
+				<h2>Bicycle</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, inventore.</p>
+			</div>
+		</div>
+	</section>
