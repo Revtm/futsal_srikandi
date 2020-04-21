580217e6fe2cecf4a779180c354bbc68b127264c
rivaldofez <rivaldofez@gmail.com>
2020-04-22 02:08:52 +0700

    membuat company profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Vehicle | Srikandi Rent</title>
+	<script type="text/javascript" src="js/jquery-3.5.0.min.js"></script>
+	<script type="text/javascript" src="js/googlemap.js"></script>
+	<link rel="stylesheet" href="style.css">
+	<style type="text/css">
+		.container {
+			height: 450px;
+			width: 100%;
+		}
+
+		#map {
+			width: 100%;
+			height: 100%;
+			border: 1px solid blue;
+		}
+
+		#data,
+		#allData {
+			display: none;
+		}
+	</style>
+</head>
+
+<body>
+
+	<header>
+		<h2><a href="#">Srikandi Rent</a></h2>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#services">Services</a></li>
+			<li><a href="#portofolio">Portofolio</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li><a href="#newsletter">Subscribe</a></li>
+			<li><a href="#maps">See Vehicles</a></li>
+			<li><a href="#list">Our Vehicles</a></li>
+		</nav>
+	</header>
