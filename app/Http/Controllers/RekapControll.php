c402ea6ebbbb51a8c5d45d2851553522349ee684
revtm <kykylizaro98@gmail.com>
2020-04-28 14:36:21 +0700

    update chart

--- app/Http/Controllers/RekapControll.php
+++ app/Http/Controllers/RekapControll.php
@@ -16,7 +16,7 @@ class RekapControll extends Controller
 {
     public function index(Request $request){
 
-      
+
       date_default_timezone_set("Asia/Jakarta");
       if(isset($request->ke)){
         // $dataRekap = RekapPenghasilan::where([['tanggal','>=', $request->dari],
@@ -41,6 +41,44 @@ class RekapControll extends Controller
       $tanggal['d'] =  $request->dari;
       $tanggal['k'] = $request->ke;
       return view('prototype_rekap', ['rekap' => $dataRekap, 'tanggal' => $tanggal, 'operator_nama'=>$request->session()->get('nama')]);
+    }
+
+    public function ubahBulan($bulan){
+      if($bulan == "Januari"){
+        return '01';
+      }else if($bulan == "Februari"){
+        return '02';
+      }else if($bulan == "Maret"){
+        return '03';
+      }else if($bulan == "April"){
+        return '04';
+      }else if($bulan == "Mei"){
+        return '05';
+      }else if($bulan == "Juni"){
+        return '06';
+      }else if($bulan == "Juli"){
+        return '07';
+      }else if($bulan == "Agustus"){
+        return '08';
+      }else if($bulan == "September"){
+        return '09';
+      }else if($bulan == "Oktober"){
+        return '10';
+      }else if($bulan == "November"){
+        return '11';
+      }else {
+        return '12';
+      }
+    }
+
+    public function getDataRekapBulanan($dari, $ke){
+      //$namabulan="April";//$request->bulan;
+      //$bulan = $this->ubahBulan($namabulan);
+      //$tahun="2020";//$request->tahun;
+      //$dataRekap['transaksi'] = Transaksi::with('jadwal','lapangan')->where('tanggal','like', $tahun.'-'.$bulan.'-'.'%')->orderBy('tanggal', 'ASC')->get();
+      $dataRekap['transaksi'] = Transaksi::with('jadwal','lapangan')->where([['tanggal','>=', $dari],
+      ['tanggal','<=', $ke]])->get();
+      return json_encode($dataRekap);
     }
 
     public function eksporExcel($dari, $ke){
